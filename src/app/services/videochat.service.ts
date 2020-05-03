09206e8e70b363981413b299b4fc782af35b2985
unknown <anderson.nunes17@etec.sp.gov.br>
2020-05-03 15:34:31 -0300

    token twilio

--- src/app/services/videochat.service.ts
+++ src/app/services/videochat.service.ts
@@ -29,17 +29,17 @@ export class VideoChatService {
     }
 
     private async getAuthToken() {
-        const auth =
-            await this.http
-                      .get<AuthToken>(`api/video/token`)
-                      .toPromise();
-
-        return auth.token;
+        const auth = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTS2YzNjJjYTU2Y2NkOTZhOGI2ZDQ5NDYxNjEzMzM3ZTBkLTE1ODg1MzA2MDkiLCJpc3MiOiJTS2YzNjJjYTU2Y2NkOTZhOGI2ZDQ5NDYxNjEzMzM3ZTBkIiwic3ViIjoiQUNkMjNlODJjZTRiNDFhYjNlODBiODYwNTc3MjE0MWI1OCIsImV4cCI6MTU4ODUzNDIwOSwiZ3JhbnRzIjp7ImlkZW50aXR5IjoiQW5kZXJzb24iLCJ2aWRlbyI6eyJyb29tIjoic2FsYS1hbmRlcnNvbiJ9fX0.migluxnZaMily9rhjFirf_JAFJLRJ2b_Rn83ZzEdHbc';
+            // await this.http
+            //           .get<AuthToken>(`http://localhost:3000/twilio/token`)
+            //           .toPromise();
+        return auth;
     }
 
     getAllRooms() {
+
         return this.http
-                   .get<Rooms>('api/video/rooms')
+                   .get<Rooms>('http://localhost:3000/twilio/rooms/DailyStandup')
                    .toPromise();
     }
 
@@ -47,6 +47,9 @@ export class VideoChatService {
         let room: Room = null;
         try {
             const token = await this.getAuthToken();
+            console.log(name);
+            console.log(tracks);
+            console.log(token);
             room =
                 await connect(
                     token, {
@@ -54,6 +57,7 @@ export class VideoChatService {
                         tracks,
                         dominantSpeaker: true
                     } as ConnectOptions);
+                    console.log(room);
         } catch (error) {
             console.error(`Unable to connect to Room: ${error.message}`);
         } finally {
